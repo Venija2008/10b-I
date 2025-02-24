f8840697cab8bf08398fb0b5e59b6eebe202909a
Venija2008 <Venija2008@gmail.com>
2025-02-24 11:22:20 +0200

    24.02.2025

---
+++ 24.02.2025/script.js
@@ -0,0 +1,84 @@
+alert("")
+let a = 5;
+let b = "5";
+
+if( a == b )
+{
+    console.log("a == b");
+}
+if( a == b )
+{
+    console.log("a === b");
+}
+
+
+let diena = parseInt(prompt("Ievadi dienas numuru"));
+
+switch( diena )
+{
+    case 1:
+        console.log("pirmndiena");
+    break;
+
+        case 2:
+            console.log("otrdiena")
+        break;
+
+        case 3:
+            console.log("trešdiena")
+        break;
+
+        case 4:
+            console.log("ceturtdiena")
+        break;
+
+        case 5:
+            console.log("piektdiena")
+        break;
+
+        case 6:
+            console.log("sestdiena")
+        break;
+
+        case 7:
+            console.log("svētdiena")
+        break;
+
+}
+
+
+//2.uzd
+let diena = parseInt(prompt("Ievadi dienas numuru"));
+
+switch( diena )
+{
+    case 1:
+        console.log("darba diena");
+    break;
+
+        case 2:
+            console.log("darba diena")
+        break;
+
+        case 3:
+            console.log("darba diena")
+        break;
+
+        case 4:
+            console.log("darba diena")
+        break;
+
+        case 5:
+            console.log("darba diena")
+        break;
+
+        case 6:
+            console.log("brīvdiena")
+        break;
+
+        case 7:
+            console.log("brīvdiena")
+        break;
+}
+
+
