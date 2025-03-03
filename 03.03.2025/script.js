693ec0a473b903c3c7fadce74448df1d67fe1cc8
Venija2008 <Venija2008@gmail.com>
2025-03-03 11:19:56 +0200

    03.03.2025uzdevumi

---
+++ 03.03.2025/script.js
@@ -0,0 +1,56 @@
+/*
+let masivs1 = [1,2,3,4,5];
+let masivs2 = [];
+
+console.log(masivs1);
+console.log(masivs1.lenght);
+console.log(masivs[0]);
+console.log(masivs[1]);
+console.log(masivs[2]);
+console.log(masivs[3]);
+console.log(masivs[4]);
+let x =4;
+console.log(masivs1[x]);
+let i = masivs1.lenght - 1;
+console.log(masivs1[i]);
+*/
+/*
+let arr = ["q","w","e","r","t","y"];
+console.log(arr);
+arr.push("u");
+console.log(arr);
+
+
+
+arr.unshift("Tab");
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.pop(); //izņem ārā pēdējo elementu
+console.log(arr);
+
+
+arr.shift();
+console.log(arr);
+
+
+arr[2] = null; //lai izņemtu trešo elementu
+console.log(arr);
+*/
+
+
+for (let i = 0; i < 5 ; i++)
+{
+    console.log(i);
+}
+for (let i = 5; i >= 0; i--)
+{
+    console.log(i);
+}
+let arr = [1,2,3,4,5,6,7,8,9,10];
+for ( let i = 0 ; i <= 9 ; i++)
+{
+    console.log(arr[i]);
+}
---
+++ 03.03.2025/script2.js
@@ -0,0 +1,17 @@
+//1.uzd
+
+let masivs1 = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < masivs1.lenght; i++)
+{
+    console.log(masivs[i]);
+}
+//2.uzd
+let vvv = 0;
+for (let i = 0; i < masivs1.lenght; i++)
+    {
+    if ( vvv < masivs1[i] ){
+       vvv = masivs1[1];
+    }
+    }
+    console.log(vvv);
